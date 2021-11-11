50f76cac8201c807aec03d1e3b02a56b1ed61a88
Jonathan Bocanegra <jbocaneg@ucsd.edu>
2021-11-10 17:25:16 -0800

    Introduction of recipeCard component

---
+++ source/assets/components/recipeCard.js
@@ -0,0 +1,127 @@
+//recipeCard.js
+
+/*********  CARD STRUCTURE ***********
+  <recipe-card>
+  ***** Shadow DOM *****
+    <section>
+      <main id='card-information'>
+        <div id='recipe-title'> </div>
+        <div id='recipe-cooking-time'> </div>
+        <div id='recipe-score'> </div>
+        <div id='recipe-servings'> </div>
+        <div id='recipe-calories'> </div>
+        <div id='recipe-total-calories'> </div>
+      </main>
+      <aside id ='card-visuals'>
+        <fig id='visual'>
+          <img id='recipe-image'/>
+          <figcaption id='recipe-dietary'>
+        </fig>
+      </aside>
+    </section> 
+  ***** END OF SHADOW DOM *****
+  <recipe-card>
+***************************************/
+
+class recipeCard extends HTMLElement{
+   constructor(){
+     super();
+     this.attachShadow({mode: 'open'});
+   }
+
+   set data(data){
+     const card = document.createElement('section');
+
+     const cardVisuals = document.createElement('aside');
+     cardVisuals.id = 'card-visuals';
+
+     const cardInformation = document.createElement('main');
+     cardInformation.id = 'card-information';
+ 
+     function setCardVisuals(){
+      
+      const visual = document.createElement('figure');
+      visual.id = 'visual';
+      /** RECIPE IMAGE */
+      const recipeImage = document.createElement('img');
+      recipeImage.id = 'recipe-image';
+      recipeImage.src = getRecipeImageSource(data);
+
+       /** RECIPE IMAGE CAPTION */
+      const recipeDietary = document.createElement('figcaption');
+      recipeDietary.id = 'recipe-dietary';
+      recipe
+
+      visual.appendChild(recipeImage);
+      visual.appendChild(recipeDietary);
+
+      cardVisuals.appendChild(visual);
+     }
+
+     function setCardInformaation(){
+      /** RECIPE TITLE */
+      const recipeTitle = document.createElement('div');
+      recipeTitle.id = 'recipe-title';
+      recipeTitle.innerHTML = getRecipeTitle(data);
+
+      /** RECIPE PRICE */
+      const recipePrice = document.createElement('div');
+      recipePrice.id = 'recipe-price';
+      recipePrice.innerHTML = getRecipePrice(data);
+
+      /** RECIPE COOKING TIME */
+      const recipeCookingTime = document.createElement('div');
+      recipeCookingTime.id = 'recipe-cooking-time';
+      recipeCookingTime.innerHTML = getRecipeCookingTime(data);
+
+      /** RECIPE SCORE */
+      const recipeScore = document.createElement('div');
+      recipeScore = 'recipe-score';
+      recipeScore.innerHTML = getRecipeScore(data);
+     
+      /** RECIPE SERVING */
+      const recipeServings = document.createElement('div');
+      recipeServings.id = 'recipe-servings';
+      recipeServings.innerHTML = getRecipeServings(data);
+
+      /** RECIPE CALORIES */
+      const recipeCalories = document.createElement('div');
+      recipeCalories.id = 'recipe-calories';
+      recipeCalories = getRecipeCalories(data);
+
+      /** RECIPE TOTAL CALORIES */
+      const recipeTotalCalories = document.createElement('div');
+      recipeTotalCalories.id = 'recipe-total-calories';
+      recipeTotalCalories = getTotalRecipeCalories(data);
+
+      cardInformation.appendChild(recipeTitle);
+      cardInformation.appendChild(recipePrice);
+      cardInformation.appendChild(recipeCookingTime);
+      cardInformation.appendChild(recipeScore);
+      cardInformation.appendChild(recipeServings);
+      cardInformation.appendChild(recipeCalories);
+      cardInformation.appendChild(recipeTotalCalories);
+     }
+
+     setCardVisuals();
+     setCardInformaation();
+
+     card.appendChild(cardVisuals);
+     card.appendChild(cardInformation);
+ 
+     this.shadowRoot.appendChild(card);
+   }
+}
+
+
+/** HELPER METHODS */
+function getRecipeTitle(data);
+function getRecipePrice(data);
+function getRecipeCookingTime(data);
+function getRecipeScore(data);
+function getRecipeCalories(data);
+function getRecipeServings(data);
+function getRecipeCalories(data);
+function getRecipeTotalCalories(data);
+
+customElements.define('recipe-card', recipeCard);
