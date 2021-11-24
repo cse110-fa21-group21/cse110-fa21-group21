81bceb3599fc9d779c432b600fa633240119ab08
Jonathan Bocanegra <jbocaneg@ucsd.edu>
2021-11-23 17:25:25 -0800

    Implementation of Dietary Feature

--- source/assets/scripts/filter.js
+++ source/assets/scripts/filter.js
@@ -8,8 +8,8 @@ export class Filter{
     //needed for filtering before new searches
     this.#goThroughElements();
 
-    const checkboxByScore = document.querySelector("form[id=filter-by-score]");
-    checkboxByScore.addEventListener("click", () => {
+    const checkboxByDiet = document.querySelector("form[id=filter-by-diet]");
+    checkboxByDiet.addEventListener("click", () => {
       // Could use this to add some more functionality to the all button.
       this.#goThroughElements();
     });
@@ -35,11 +35,10 @@ export class Filter{
     recipeCards.forEach((card) => {
         card.classList.add("shown");
         card.classList.remove("hidden");
-        this.#scoreAllChecked();
         this.#priceAllChecked();
         this.#timeAllChecked();
-        if (this.#checkScoreChecked()) {
-          this.#filterByScore(card);
+        if (this.#checkDietaryChecked()) {
+          this.#filterByDietary(card);
         }
         if (this.#checkPriceChecked()) {
           this.#filterByPrice(card);
@@ -55,49 +54,55 @@ export class Filter{
    * @param {recipeCard} recipeCard
    * @private
    */
-  #filterByScore(recipeCard) {
+  #filterByDietary(recipeCard) {
     //parse recipe score
-    const recipeScore = this.#extractScore(
-      recipeCard.shadowRoot.getElementById("recipe-score").innerHTML
-    );
-    
-    const ranges = [];
-    const checks = [];
+    const recipeDietary = this.#extractDietary(recipeCard);
+
     /**
      * determine in which range our element falls into, the ranges
      * by design must be mutually exclusive
      */
-    const first_score = recipeScore >= 75;
-    const second_score = recipeScore >= 50 && recipeScore < 75;
-    const third_score = recipeScore >= 25 && recipeScore < 50;
-    const fourth_score = recipeScore >= 0 && recipeScore < 25;
+    const dairy_free_diet = recipeDietary["dairy-free"];
+    const gluten_free_diet = recipeDietary["gluten-free"];
+    const vegetarian_diet = recipeDietary["vegetarian"];
+    const vegan_diet = recipeDietary["vegan"];
     
-    ranges.push(first_score);
-    ranges.push(second_score);
-    ranges.push(third_score);
-    ranges.push(fourth_score);
 
     /**
      * determine which ranges are being filtered for
      *  - true: we want to see items in this range
      *  - false: we do not want to see item in this range
      */
-    const first_checked = document.querySelector("input[id=first-score]").checked;
-    const second_checked = document.querySelector("input[id=second-score]").checked;
-    const third_checked = document.querySelector("input[id=third-score]").checked;
-    const fourth_checked = document.querySelector("input[id=fourth-score]").checked;
+    const dairy_free_checked = document.querySelector("input[id=dairy-free-diet]").checked;
+    const gluten_free_checked = document.querySelector("input[id=gluten-free-diet]").checked;
+    const vegetarian_checked = document.querySelector("input[id=vegetarian-diet]").checked;
+    const vegan_checked = document.querySelector("input[id=vegan-diet]").checked;
     
-    checks.push(first_checked);
-    checks.push(second_checked);
-    checks.push(third_checked);
-    checks.push(fourth_checked);
 
     /**
      * if an item falls into a range but we are not requesting to see
      * it then it should be hidden
      */
-    this.#decider(recipeCard,ranges,checks);
-  }
+    if(dairy_free_checked && !dairy_free_diet){
+      recipeCard.classList.add('hidden');
+      recipeCard.classList.remove('shown');
+    }
+
+    if(gluten_free_checked && !gluten_free_diet){
+      recipeCard.classList.add('hidden');
+      recipeCard.classList.remove('shown');
+    }
+
+    if(vegetarian_checked && !vegetarian_diet){
+      recipeCard.classList.add('hidden');
+      recipeCard.classList.remove('shown');
+    }
+    
+    if(vegan_checked && !vegan_diet){
+      recipeCard.classList.add('hidden');
+      recipeCard.classList.remove('shown');
+    }
+ }
   /**
    * We check whether or not this recipe card falls into a range
    * that we have requested not to see. If so, we hide it.
@@ -205,22 +210,6 @@ export class Filter{
     this.#decider(recipeCard,ranges,checks)
   }
 
-
-  /**
-   * Ensures that no other filter within scores can be checked 
-   * when the 'all' filter is selected
-   * @private
-   */
-  #scoreAllChecked() {
-    const all_checked = document.querySelector("input[id=all-score]").checked;
-    // ensure no other checkbox within the same form can be checked if 'all' is checked
-    if (all_checked) {
-      document.querySelector("input[id=first-score]").checked = false;
-      document.querySelector("input[id=second-score]").checked = false;
-      document.querySelector("input[id=third-score]").checked = false;
-      document.querySelector("input[id=fourth-score]").checked = false;
-    }
-  }
   /**
    * Ensures that no other filter within prices can be checked 
    * when the 'all' filter is selected
@@ -254,16 +243,15 @@ export class Filter{
   }
 
   /**
-   * Checks if any filter, excluding the 'all' filter, is checked within 
-   * scores
+   * Checks if any filter is checked within dietary restrictions
    * @returns {boolean} true -- if at least one of the checkboxes is checked
    * @returns {boolean} false -- otherwise
    */
-  #checkScoreChecked() {
-    const first_checked = document.querySelector("input[id=first-score]").checked;
-    const second_checked = document.querySelector("input[id=second-score]").checked;
-    const third_checked = document.querySelector("input[id=third-score]").checked;
-    const fourth_checked = document.querySelector("input[id=fourth-score]").checked;
+  #checkDietaryChecked() {
+    const first_checked = document.querySelector("input[id=dairy-free-diet]").checked;
+    const second_checked = document.querySelector("input[id=gluten-free-diet]").checked;
+    const third_checked = document.querySelector("input[id=vegetarian-diet]").checked;
+    const fourth_checked = document.querySelector("input[id=vegan-diet]").checked;
 
     return (first_checked || second_checked || third_checked || fourth_checked);
   }
@@ -305,7 +293,8 @@ export class Filter{
     * Check to see which range the card falls into and if the coinciding check is false,
     * (a.k.a not requested) then we hide the card
     * @param {recipeCard} card  - the DOM in question
-    * @param {boolean[]} ranges - only one element in the ranges is set to true by design
+    * @param {boolean[]} ranges - only one element in the ranges is set to true by design, 
+    *                             (mutual exclusivity required)
     * @param {boolean[]} checks - reflective of the current selected checkboxes
     * @private
     */
@@ -337,10 +326,13 @@ export class Filter{
    * @returns {Number} recipe spoonacular score
    * @private
    */
-  #extractScore(scoreString) {
-    let slashIndex = scoreString.indexOf("/");
-    let value = scoreString.substring(7, slashIndex);
-    return Number(value);
+  #extractDietary(recipeCard) {
+     const dietary = {};
+     dietary["dairy-free"] = !(recipeCard.shadowRoot.getElementById('dairy-free').hidden);
+     dietary["gluten-free"] = !(recipeCard.shadowRoot.getElementById('gluten-free').hidden)
+     dietary["vegetarian"] = !(recipeCard.shadowRoot.getElementById('vegetarian').hidden);
+     dietary["vegan"] = !(recipeCard.shadowRoot.getElementById('vegan').hidden);
+     return dietary; 
   }
   /**
    * Parse the numerical price from the spoonacular.js 
